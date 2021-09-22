604a12356238c7c079bc702ce44ac8ac4b9801a4
Jack Yu <jackchungchiehyu@gmail.com>
2021-09-22 22:11:03 +0800

    Import missing modules

--- helmet_tracker/models/deepsort_postprocess.py
+++ helmet_tracker/models/deepsort_postprocess.py
@@ -1,10 +1,16 @@
 
 
 import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import cv2
 from tqdm.auto import tqdm
 from deep_sort.deep_sort import DeepSort
 from utils.parser import get_config
+
+from helmet_tracker.util import NFLAssignmentScorer
+
 
 
 
