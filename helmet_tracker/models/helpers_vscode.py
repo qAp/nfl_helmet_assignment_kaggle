03f7eaee9b70341ed00e24f8b96c6b2112a2f0e0
Jack Yu <jackchungchiehyu@gmail.com>
2021-09-22 12:39:15 +0800

    Debuggin import
    Create a dummy module that imports definitions from
    yolov5_deepsort_pytorch, to see if the locally defined
    functions will be able to be imported in Kaggle Notebook.

---
+++ helmet_tracker/models/helpers_vscode.py
@@ -0,0 +1,11 @@
+import easydict
+from deep_sort.deep_sort import DeepSort
+from utils.parser import get_config
+
+
+def deepsort_helmets():
+    print('Deepsort helmets :D')
+
+
+def lol_config():
+    return get_config()
