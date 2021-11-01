98fa87006dd1c6f19e1828dd131a1b4d7f53584d
Jack Yu <jackchungchiehyu@gmail.com>
2021-11-02 03:26:23 +0800

    Refactor FairMOT postprocessing
    More carefully handle duplicated labels and missing labels.

--- helmet_tracker/models/fairmot_postprocess.py
+++ helmet_tracker/models/fairmot_postprocess.py
@@ -104,15 +104,48 @@ def assign_player_to_track(df):
             )
 
     cluster2player = grpd.first()['label'].to_dict()
-    cluster2count = grpd.first()['label_count'].to_dict()
+    cluster2player_count = grpd.first()['label_count'].to_dict()
+    cluster2player_total_count = grpd['label_count'].sum().to_dict()
 
     df['label_fairmot'] = df['fairmot_cluster'].map(cluster2player)
-    df['label_count_fairmot'] = df['fairmot_cluster'].map(cluster2count)
+    df['label_count_fairmot'] = df['fairmot_cluster'].map(cluster2player_count)
+    df['label_cluster_count_fairmot'] = df['fairmot_cluster'].map(
+        cluster2player_total_count)
+    df['label_perct_fairmot'] = df['label_count_fairmot'] / \
+        df['label_cluster_count_fairmot']
 
     return df
 
 
-def fmot_postprocess_hmap(df_hmap, pth_fmot, pth_video=None):
+def remove_duplicated_label_fairmot(df_tracks, keep_by='label_perct_fairmot'):
+    '''
+    For duplicated labels in the same video frame, keep one and discard the rest.
+    
+    Args:
+        df_tracks (pd.DataFrame): Each row a helmet in some video frame. 
+        keep_by (str): Options are:
+            'label_perct_fairmot': Keep the label that takes up the largest
+                percentage in its MOT cluster.
+            'label_count_fairmot': Keep the label that has the largest
+                number of count.
+            Default: 'label_perct_fairmot'
+            
+    Returns:
+        df (pd.DataFrame): Like input, but in each video frame, the labels
+            are unique.
+    '''
+    df = df_tracks.copy()
+    df.sort_values(keep_by, ascending=False, inplace=True)
+    is_duped = df.duplicated(
+        subset=['video_frame', 'label_fairmot'], keep='first')
+    return df[~is_duped]
+
+
+def fmot_postprocess_hmap(df_hmap, pth_fmot,
+                          pth_video=None,
+                          drop_dupe_id=True,
+                          keep_by='label_perct_fairmot',
+                          hmap_fills_missing=True):
     '''
     Postprocess helmet mapping results using FairMOT tracks,
      for a video.
@@ -124,10 +157,25 @@ def fmot_postprocess_hmap(df_hmap, pth_fmot, pth_video=None):
             the video is loaded and width and height are extracted,
             to be used to rescale the FairMOT bboxes.
             
+        drope_dupe_id (bool): If `True`, drop helmet in a video frame if
+            it's labelled with a duplicated FairMOT id.
+            
+        keep_by (str): Options are:
+            'label_perct_fairmot': Keep the label that takes up the largest
+                percentage in its MOT cluster.
+            'label_count_fairmot': Keep the label that has the largest
+                number of count.
+            Default: 'label_perct_fairmot'            
+            
+        hmap_fills_missing (bool): If `True`, fill missing FairMOT labels
+            with helmet mapping labels.  Default: True
+            
     Returns: 
         df_tracks (pd.DataFrame): Helmets assigned with player numbers
             that taking into account both helmet mapping and FairMOT,
-            for a video.
+            for a video.  In each video frame, all labels are unique, but
+            there may be fewer helmets than there are in helmet mapping
+            results.
     '''
     if pth_video is not None:
         cap = cv2.VideoCapture(pth_video)
@@ -143,11 +191,35 @@ def fmot_postprocess_hmap(df_hmap, pth_fmot, pth_video=None):
         hmap = df_hmap[df_hmap['frame'] == frame].copy()
         fmot = df_fmot[df_fmot['frame'] == frame].copy()
 
-        merged = merge_hmap_fmot_bbox(hmap, fmot, drop_dupe_id=False)
+        merged = merge_hmap_fmot_bbox(hmap, fmot, drop_dupe_id)
         df_tracks.append(merged)
 
     df_tracks = pd.concat(df_tracks, axis=0)
 
     df_tracks = assign_player_to_track(df_tracks)
+
+    # This removes duplicates resulting from fairmot postprocessing
+    df_tracks = remove_duplicated_label_fairmot(df_tracks, keep_by)
+
+    if hmap_fills_missing:
+        # Use helmet mapping label where fairmot postprocessing
+        # has not provided one, assigning these backup labels
+        # as having a `label_count_fairmot` equal to 0.
+        cols_merge = ['video_frame',
+                      'left', 'width', 'top', 'height',
+                      'label', 'video', 'frame']
+        df_tracks = pd.merge(df_hmap, df_tracks,
+                             left_on=cols_merge,
+                             right_on=cols_merge,
+                             how='outer')
+
+        df_tracks['label_fairmot'].fillna(df_tracks['label'], inplace=True)
+        df_tracks[keep_by].fillna(1e-5, inplace=True)
+
+        # Again, there may still be some duplicated labels.  Remove them once again.
+        df_tracks = remove_duplicated_label_fairmot(df_tracks, keep_by)
+
+    assert df_tracks.duplicated(
+        subset=['video_frame', 'label_fairmot']).sum() == 0
 
     return df_tracks
